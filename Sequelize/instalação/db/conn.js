c5038995769d9f968a84212cd8e90f989c6d884c
André Luiz Belmonte <belmonte_al@outlook.com>
2024-02-13 18:38:56 -0300

    Trabalhando com Serialize

---
+++ Sequelize/instalação/db/conn.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+const sequilize = new Sequelize("nodesequelize2", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+try {
+  sequilize.authenticate();
+  console.log("Conectamos com sucesso com o Sequilize!");
+} catch (err) {
+  console.log("Não foi possível conectar:", error);
+}
+
+module.exports = sequilize;
